54eddd200c355fa4bc2df00273b5253e82fc6a01
Carl David Brown JR <carldavidbrown@gmail.com>
2022-10-08 19:43:49 -0400

    Software1-Knowledge-Check-1 initial commit

---
+++ Software1-Knowledge-Check-1/Software1-Knowledge-Check-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Software1_Knowledge_Check_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
