91b3842dadaf06116521d73c88fda1bf44fa9e55
Carl David Brown JR <carldavidbrown@gmail.com>
2022-10-08 19:51:25 -0400

    Importation of Previously Existing Source Code:  https://github.com/JakeSaintG/Software1-Knowledge-Check-1

---
+++ Software1-Knowledge-Check-1/Software1-Knowledge-Check-1/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software1_Knowledge_Check_1
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
